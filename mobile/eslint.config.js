5a4261ac34d5873f8324b33aec6ffcd89d184468
Amal Manoj <amalmanoj.official@gmail.com>
2026-08-29 21:39:29 +0530

    chore(mobile): exclude Figma plugin from app lint

--- mobile/eslint.config.js
+++ mobile/eslint.config.js
@@ -4,6 +4,6 @@ const expoConfig = require('eslint-config-expo/flat');
 module.exports = defineConfig([
   ...expoConfig,
   {
-    ignores: ['dist/**', 'coverage/**', '.expo/**'],
+    ignores: ['dist/**', 'coverage/**', '.expo/**', 'figma-dawnage-design-system/**'],
   },
 ]);
